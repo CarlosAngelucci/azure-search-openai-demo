0369d506525d1af1e02f47e52677bedaa6415cc0
CarlosAngelucci <109881362+CarlosAngelucci@users.noreply.github.com>
2024-02-21 12:30:35 +0000

    Alteração do texto padrão no header

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -14,7 +14,7 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
                     <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}>GPT + Enterprise data | Sample</h3>
+                        <h3 className={styles.headerTitle}>ChatBot de Viagens - Vale AI Center</h3>
                     </Link>
                     <nav>
                         <ul className={styles.headerNavList}>
