896717119c4cc252d05f812b6e5da9ea1ff1adf3
CarlosAngelucci <carlosangeluccimarchiori@gmail.com>
2024-02-22 00:27:01 +0000

    Testando a função dos botões de like e dislike

--- app/frontend/src/components/Answer/AnswerError.tsx
+++ app/frontend/src/components/Answer/AnswerError.tsx
@@ -1,7 +1,9 @@
-import { Stack, PrimaryButton } from "@fluentui/react";
+import { IIconProps, Stack, PrimaryButton } from "@fluentui/react";
 import { ErrorCircle24Regular } from "@fluentui/react-icons";
+import { IconButton } from "@fluentui/react/lib/Button";
 
 import styles from "./Answer.module.css";
+import { useState } from "react";
 
 interface Props {
     error: string;
@@ -9,6 +11,24 @@ interface Props {
 }
 
 export const AnswerError = ({ error, onRetry }: Props) => {
+    const [clickedLikeButton, setClickedLikeButton] = useState<boolean>(false);
+    const [clickedDislikeButton, setClickedDislikeButton] = useState<boolean>(false);
+
+    const dislikeIcon: IIconProps = { iconName: clickedDislikeButton ? "DislikeSolidIcon" : "DislikeIcon" };
+    const likeIcon: IIconProps = { iconName: clickedLikeButton ? "LikeSolidIcon" : "LikeIcon" };
+
+    function onClickDislike(): void {
+        alert("Cliquei Dislike");
+        setClickedDislikeButton(true);
+        setClickedLikeButton(false);
+    }
+
+    function onClickLike(): void {
+        alert("Cliquei Like");
+        setClickedLikeButton(true);
+        setClickedDislikeButton(false);
+    }
+
     return (
         <Stack className={styles.answerContainer} verticalAlign="space-between">
             <ErrorCircle24Regular aria-hidden="true" aria-label="Error icon" primaryFill="red" />
@@ -16,8 +36,13 @@ export const AnswerError = ({ error, onRetry }: Props) => {
             <Stack.Item grow>
                 <p className={styles.answerText}>{error}</p>
             </Stack.Item>
-
-            <PrimaryButton className={styles.retryButton} onClick={onRetry} text="Retry" />
+            <Stack className={styles.teste} horizontal>
+                <PrimaryButton className={styles.retryButton} onClick={onRetry} text="Retry" />
+                <Stack horizontal>
+                    <IconButton iconProps={dislikeIcon} title="Emoji" ariaLabel="Emoji" checked={checked} onClick={onClickDislike} />
+                    <IconButton iconProps={likeIcon} title="Emoji" ariaLabel="Emoji" checked={checked} onClick={onClickLike} />
+                </Stack>
+            </Stack>
         </Stack>
     );
 };
