b3850091055246680a17164d637c7d9a202b23e2
CarlosAngelucci <carlosangeluccimarchiori@gmail.com>
2024-02-29 21:52:29 -0300

    textfield feedback

--- app/frontend/src/components/Answer/LikeDislikeButton.tsx
+++ app/frontend/src/components/Answer/LikeDislikeButton.tsx
@@ -1,8 +1,38 @@
-import { IIconProps } from "@fluentui/react";
-import { IconButton } from "@fluentui/react/lib/Button";
+import { IIconProps, Stack } from "@fluentui/react";
+import { IconButton, PrimaryButton } from "@fluentui/react/lib/Button";
 import { useState } from "react";
+import * as React from 'react';
+import { mergeStyleSets, DefaultButton, FocusTrapZone, Layer, Overlay, Popup } from '@fluentui/react';
+import { useBoolean } from '@fluentui/react-hooks';
+import { TextField } from '@fluentui/react/lib/TextField';
+
+
+const popupStyles = mergeStyleSets({
+    root: {
+      background: 'rgba(0, 0, 0, 0.2)',
+      bottom: '0',
+      left: '0',
+      position: 'fixed',
+      right: '0',
+      top: '0',
+    },
+    content: {
+      background: 'white',
+      left: '50%',
+      maxWidth: '400px',
+      padding: '0 2em 2em',
+      position: 'absolute',
+      top: '50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  });
+  
 
 export const Like = () => {
+    const [textoFeedback, { setTrue: showTextoFeedback, setFalse: hideTextoFeedback }] = useBoolean(false);
+    const [motivoFeedback, setMotivoFeedback] = useState<string>("")
+    const [multiline, { toggle: toggleMultiline }] = useBoolean(false);
+
     const [clickedLikeButton, setClickedLikeButton] = useState<boolean>(false);
     const [clickedDislikeButton, setClickedDislikeButton] = useState<boolean>(false);
 
@@ -10,7 +40,7 @@ export const Like = () => {
     const likeIcon: IIconProps = { iconName: clickedLikeButton ? "LikeSolid" : "Like" };
 
     function onClickDislike(): void {
-        alert("Cliquei Dislike");
+        showTextoFeedback();
         setClickedDislikeButton(true);
         setClickedLikeButton(false);
     }
@@ -21,10 +51,49 @@ export const Like = () => {
         setClickedDislikeButton(false);
     }
 
+    const onChange = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+        setMotivoFeedback(newValue || '');
+      };
+
+    function onSubmit(): void {
+        alert("Feedback enviado")
+    }
+
+    function onCancelar(): void {
+        setMotivoFeedback("")
+        hideTextoFeedback()
+    }
+
+
     return (
         <div>
+            {textoFeedback && (
+                <Layer>
+                  <Popup
+                    className={popupStyles.root}
+                    role="dialog"
+                    aria-modal="true"
+                    onDismiss={hideTextoFeedback}
+                    enableAriaHiddenSiblings={true}
+                  >
+                    <Overlay onClick={hideTextoFeedback} />
+                    <FocusTrapZone>
+                      <div role="document" className={popupStyles.content}>
+                        <h2>Feedback da Respota</h2>
+                        <TextField label="Nos diga o motivo pelo qual a resposta não foi satisfatória." multiline={multiline} value={motivoFeedback} onChange={onChange}/>
+                        <Stack>
+                            <DefaultButton text="Cancelar" onClick={onCancelar} allowDisabledFocus />
+                            <PrimaryButton text="Enviar" onClick={onSubmit} allowDisabledFocus  />
+                        </Stack>
+                        <DefaultButton onClick={hideTextoFeedback}>Close Popup</DefaultButton>
+                      </div>
+                    </FocusTrapZone>
+                  </Popup>
+                </Layer>
+            )}
             <IconButton iconProps={dislikeIcon} title="Dislike" ariaLabel="EmojiDislike" onClick={onClickDislike} />
             <IconButton iconProps={likeIcon} title="Like" ariaLabel="EmojiLike" onClick={onClickLike} />
+            
         </div>
     );
 };
